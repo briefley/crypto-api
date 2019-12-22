399f2445525104bd797d4e2f269c3f7606d9bb25
Lasha Lomidze <lomidzelf@gmail.com>
2019-12-22 22:30:30 +0400

    Implement request body validation function Convert exchangeFrom and ExchangeTo to lower case for easier processing

--- app/Http/Controllers/Currency/CurrencyController.php
+++ app/Http/Controllers/Currency/CurrencyController.php
@@ -15,11 +15,54 @@ class CurrencyController extends Controller
      * @param Request $request
      * @param $exchangeFrom
      * @param $exchangeTo
-     * @return bool
+     * @return false|string
      * @author Lasha Lomidze <lomidzelf@gmail.com>
+     * Entry point function routed to by an API route.
      */
     public function analyseExchangeWays(Request $request, $exchangeFrom, $exchangeTo)
     {
-        return true;
+        $validationResult = $this->validateRequestBody($request);
+        $exchangeFrom = strtolower($exchangeFrom);
+        $exchangeTo = strtolower($exchangeTo);
+        return json_encode($validationResult);
+    }
+
+    /**
+     * @param $request
+     * @return array
+     * @author Lasha Lomidze <lomidzelf@gmail.com>
+     * Handles the validation of the API request body parameter.
+     * Firstly, checks if the request body can be converted to JSON,
+     * then moves to validation by rules, so all required parameters are present.
+     */
+    private function validateRequestBody($request)
+    {
+        $rules = [
+            '*' => 'required',
+            '*.currencyFrom' => 'required|string',
+            '*.currencyTo' => 'required|string',
+        ];
+
+        if (!$request->json()->all()) {
+            return [
+                'result' => false,
+                'errors' => 'Invalid JSON Passed'
+            ];
+        }
+
+        $validator = Validator::make($request->json()->all(), $rules);
+
+        if ($validator->passes()) {
+            return [
+                'result' => true,
+                'errors' => null
+            ];
+        }
+
+        return [
+            'result' => false,
+            'errors' => $validator->errors()->toJson()
+        ];
+
     }
 }
