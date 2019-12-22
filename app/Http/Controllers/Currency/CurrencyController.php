ac3f72f041c75174340466777f75c6e452a69202
Lasha Lomidze <lomidzelf@gmail.com>
2019-12-22 23:08:30 +0400

    Implement search of all possible ways of connecting two currencies using BFS algorithm

--- app/Http/Controllers/Currency/CurrencyController.php
+++ app/Http/Controllers/Currency/CurrencyController.php
@@ -24,7 +24,72 @@ class CurrencyController extends Controller
         $validationResult = $this->validateRequestBody($request);
         $exchangeFrom = strtolower($exchangeFrom);
         $exchangeTo = strtolower($exchangeTo);
+        if ($validationResult['result']) {
+            $exchangeWaysArr = $request->json()->all();
+            $result = [];
+            $trackedWays = [];
+
+            $this->findExchangeWays($exchangeWaysArr, $exchangeFrom, $exchangeTo, $result, $trackedWays, $totalResults);
+            return json_encode($totalResults);
+        }
+
         return json_encode($validationResult);
+    }
+
+    /**
+     * @param $exchangeWaysArr
+     * @param $exchangeFrom
+     * @param $exchangeTo
+     * @param $result
+     * @param $trackedWays
+     * @param $totalResults
+     * @author Lasha Lomidze <lomidzelf@gmail.com>
+     * Uses backtracking algorithm to find all possible currency exchange ways
+     * in a provided dataset.
+     */
+    private function findExchangeWays($exchangeWaysArr, $exchangeFrom, $exchangeTo, $result, $trackedWays, &$totalResults)
+    {
+        /*
+         * Checks if the final destination is reached
+         * and saves it in an array passed by reference.
+         */
+        if ($exchangeFrom == $exchangeTo) {
+            $totalResults[] = $result;
+            return;
+        }
+
+        /*
+         * Loops through all provided exchange way pairs
+         * of currencies. Essentially moves in breadth oriented way.
+         */
+        for ($i = 0; $i < count($exchangeWaysArr); $i++) {
+            /*
+             * Checks if the currently iterated element equals to the element
+             * from which we want to exchange
+             * and also checks if the current way wasn't used before.
+             */
+            if ($exchangeWaysArr[$i]['currencyFrom'] == $exchangeFrom && !isset($trackedWays[$i])) {
+                /*
+                 * Adds on the new steps to result.
+                 */
+                $currResult = $result;
+                $result[] = $exchangeWaysArr[$i];
+                /*
+                 * Saves already passed steps for backtracking.
+                 */
+                $currTrackedWays = $trackedWays;
+                $trackedWays[$i] = true;
+                /*
+                 * Recursive call for moving down in a tree.
+                 */
+                $this->findExchangeWays($exchangeWaysArr, $exchangeWaysArr[$i]['currencyTo'], $exchangeTo, $result, $trackedWays, $totalResults);
+                /*
+                 * Resets state for next iteration of the loop.
+                 */
+                $result = $currResult;
+                $trackedWays = $currTrackedWays;
+            }
+        }
     }
 
     /**
